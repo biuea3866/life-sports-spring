dd97b5a8c7ff8f80026e9457a69ecd471b842838
biuea <biuea@doodlin.co.kr>
2022-12-29 14:15:28 +0900

    add server: user-server

---
+++ user-server/src/main/kotlin/biuea/lifesports/userserver/UserServerApplication.kt
@@ -0,0 +1,13 @@
+package biuea.lifesports.userserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+
+@SpringBootApplication
+@EnableDiscoveryClient
+class UserServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<UserServerApplication>(*args)
+}
---
+++ user-server/src/test/kotlin/biuea/lifesports/userserver/UserServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package biuea.lifesports.userserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
