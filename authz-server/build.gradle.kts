9d5f9f3c22df4eefe60b9ef850a23ff325c6dd02
biuea <biuea@doodlin.co.kr>
2022-12-30 20:56:07 +0900

    fix
    fix

--- authz-server/build.gradle.kts
+++ authz-server/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     runtimeOnly("mysql:mysql-connector-java")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
